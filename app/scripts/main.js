3617ceed770741b698eed821293a34bd3abf9c11
Jeremy Crawford <jcrawford.design@gmail.com>
2014-03-18 17:18:40 -0400

    starting Parse in main.js

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -15,3 +15,70 @@ $('.sign-in-btn').click(function() {
 
 
 Parse.initialize("3iYwTMywkleelnVUpiZdaXPPqVpvDsxw5qsJ1pev", "xLRITPtlktBrAiar3Xfk83Rs1isM23IbUHdXbr82");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+$('.save-button-bar').click(function){
+  console.log('bar button firing')
+  var bar = new Parse.User();
+
+  var barUsername $('.bar-username').val();
+  var barLocation $('.bar-location').val();
+  var barPhone $('.bar-phone').val();
+  var barEmail $('.bar-email').val();
+  var barPassword $('.bar-password').val();
+  var barVerifyPassword $('.bar-verify-password').val();
+
+
+
+
+  bar.set("username", "my name");
+  bar.set("password", "my pass");
+  bar.set("email", "email@example.com");
+  bar.set("email", "email@example.com");
+  bar.set("email", "email@example.com");
+  bar.set("email", "email@example.com");
+   
+  // other fields can be set just like with Parse.Object
+  bar.set("phone", "415-392-0202");
+   
+  bar.signUp(null, {
+    success: function(bar) {
+      // Hooray! Let them use the app now.
+    },
+    error: function(bar, error) {
+      // Show the error message somewhere and let the bar try again.
+      alert("Error: " + error.code + " " + error.message);
+    }
+  });
+}
