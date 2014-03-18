a1177a7dcdee57d9ad9d48b69606f04a70c8d676
Shane Sniteman <ssniteman@gmail.com>
2014-03-18 14:09:05 -0400

    added sign-in view and jquery inside of that)

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,14 +1,13 @@
 // sign in button drop down 
 
-$('.sign-in-btn').click(function() {
-    $(this).addClass('active');
-    $('.sign-in-drop').addClass('active');
-    $('.create-account-btn').addClass('active');
-    $('.enter-btn').addClass('active');
+// $('.sign-in-btn').click(function() {
+//     $(this).addClass('active');
+//     $('.sign-in-drop').addClass('active');
+//     $('.create-account-btn').addClass('active');
+//     $('.enter-btn').addClass('active');
 
 
-});
-
+// });
 
 
 
