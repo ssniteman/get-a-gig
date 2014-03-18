365f36d24b5d7b62531a159bce2607dc88c6de80
Shane Sniteman <ssniteman@gmail.com>
2014-03-17 23:33:51 -0400

    sign-in drop works via jquery

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,7 @@
+$('.sign-in').click(function() {
+    $('.sign-in-drop').addClass('active');
+});
+
+
+
 Parse.initialize("3iYwTMywkleelnVUpiZdaXPPqVpvDsxw5qsJ1pev", "xLRITPtlktBrAiar3Xfk83Rs1isM23IbUHdXbr82");
