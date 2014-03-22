1342e243b195a0003d77dc0ee288b5a36b571769
Shane Sniteman <ssniteman@gmail.com>
2014-03-22 11:44:52 -0400

    created new Views folder for your profile - keeping separate

--- app/scripts/ViewsPreProfile.js
+++ app/scripts/ViewsPreProfile.js
@@ -131,13 +131,3 @@ var CreateAccountView = Parse.View.extend({
         new YourBarProfileView()
     }
 });
-
-////////////////Your Musician Profile View////////////////////
-
-// var YourMusicianProfileView = Parse.View.extend({
-
-//     renderedTemplate: _.template($('#your-musician-profile-template').text()),
-
-
-
-// })
