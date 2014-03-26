ea9609b093ffd34453843676e791d9507699e5a6
Jeremy Crawford <jcrawford.design@gmail.com>
2014-03-26 19:22:30 -0400

    connected form inputs to parse users

--- app/scripts/ViewsEditProfile.js
+++ app/scripts/ViewsEditProfile.js
@@ -33,7 +33,31 @@ var EditMusicianProfileView = Parse.View.extend({
 
         // setting inputs into Parse as objects
 
+        musician.set('musicbandname', $('.musician-name-form').val());
+        musician.set('email', $('.musician-email-form').val());
+        musician.set('city', $('.musician-city-form').val());
+        musician.set('genre', $('.musician-genre-form').val());
+        musician.set('nightlyrate', $('.musician-nightly-rate-form').val());
+        musician.set('availability', $('.musician-availability-form').val());
+        musician.set('userType', "musician");
 
+        console.log('pushing to parse')
+
+        musician.signUp(null, {
+            success: function(musician) {
+                // window.location.href = '/#musicianprofile'
+                new YourMusicianProfileView({
+                    model: musician
+                })
+            },
+            error: function(musician, error) {
+                // Show the error message somewhere and let the user try again.
+                alert("Error: " + error.code + " " + error.message);
+            }
+        });
+
+
+        console.log('firing musician edit profile view')
 
     }
 
